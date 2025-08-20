f9905843340c473e5b7d09ec253beb0cd6ca45bd
JardonB <jordan.breitling@gmail.com>
2025-08-19 19:54:21 -0600

    Modified main.py

--- main.py
+++ main.py
@@ -4,20 +4,24 @@ from player import *
 
 def main():
     pygame.init()
-    print(f"Starting Asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    Player.containers = (updatables, drawables)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) #set display mode
     clock = pygame.time.Clock()
     dt = 0
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
-
+    
     while True: #game loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT: #if quit event type
                 return                    #return nothing, quit program
         
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        for updatable in updatables:
+            updatable.update(dt)
+        for drawable in drawables:
+            drawable.draw(screen)
         pygame.display.flip()
 
         #limit to  60 fps
